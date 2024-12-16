c30a69872b3d70c52ee2d707c527e1d0c7acbc21
Roger Earl <rogerearl@me.com>
2024-12-15 22:44:06 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
