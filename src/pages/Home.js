8975c3ebfbe2d90f0deaa5e5b3c9d0390f122c35
Roger Earl <rogerearl@me.com>
2025-03-05 22:29:56 -0800

    Fixed image on home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import "../styles/Home.css";
 import { NavLink } from "react-router-dom";
+import rogerImage from "../assets/images/roger.png";
 
 function Home() {
   return (
     <div className="home-container">
       <section className="hero-section">
         <h1>Roger Earl</h1>
-        <image src="../assets/images/roger.png" />
+        <img src={rogerImage} alt="Roger Earl" className="profile-image" />
         <h2>Full Stack Web Developer</h2>
         <p className="intro-text">
           Building modern web applications with a focus on clean code and user
